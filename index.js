d6f9b9b1c017efd81442e85ba19d2d9f8620d8b7
Cara Morelli <caramorelli7@gmail.com>
2018-11-17 02:13:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,4 @@
 
 const currentUser = 'Grace Hopper';
  
-// const welcomeMessage = currentUser;
-const welcomeMessage = 'Welcome to Flatbook, currentUser';
-typeof "const currentUser = 'Grace Hopper';";
-//=> "string"
- 
-"const currentUser = 'Grace Hopper';".length;
-//=> 35
- 
-currentUser;
-//=> Uncaught ReferenceError: currentUser is not defined
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
